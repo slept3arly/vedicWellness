a8050b30ee4a2e324c8f4b11842566691ccde6d6
Vinayak <vinayaknautiyal38@gmail.com>
2026-01-04 04:00:38 +0530

    update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export default function About() {
   return (    
-    <div className="flex flex-col min-h-screen items-center gap-3 py-25 px-4">
+    <div className="flex flex-col min-h-screen items-center gap-3 py-40 px-4">
       
       <motion.h1
         initial={{ opacity: 0, y: 30 }}
