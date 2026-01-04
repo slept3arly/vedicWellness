ea07f6fe824e14cca46d3091dbbf33011bec5e91
Vinayak <vinayaknautiyal38@gmail.com>
2026-01-04 13:00:13 +0530

    update

---
+++ app/fonts.ts
@@ -0,0 +1,32 @@
+import { Inter, Playfair_Display, Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
+
+export const geistSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+  display: "swap",
+});
+
+export const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+  display: "swap",
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+export const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+  display: "swap",
+});
+
+export const notoSerifExtraCondensed = localFont({
+  src: "../public/fonts/NotoSerif_ExtraCondensed-Regular.woff2",
+  variable: "--font-noto-serif-extra-condensed",
+  display: "swap",
+});
