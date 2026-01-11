0e6872d90019c31b5e92ea25b27aefea84afad1a
Vinayak <vinayaknautiyal38@gmail.com>
2026-01-11 13:10:13 +0530

    update

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-<div className="lg:pt-8 px-4 lg:px-28 ">
+<div className="text-black dark:text-white font-bold lg:pt-8 px-4 lg:px-28 ">
     <div className="flex flex-row justify-between items-center">
-      <div className="flex px-4 py-1 gap-3 bg-neutral-800/90 rounded-lg">
+      <div className="flex px-4 py-1 gap-3 rounded-lg">
           <Link href="/admin">
             Admin Panel
           </Link>
       </div>
-      <div className="flex flex-col lg:flex-row lg:gap-3 px-4 py-1 gap-1 bg-neutral-800/90 rounded-lg">
+      <div className="flex flex-col lg:flex-row text-right lg:gap-3 px-4 py-1 gap-1 text-black dark:text-white rounded-lg">
         <Link href="/admin/products">Products</Link>
         <Link href="/admin/blogs">Blogs</Link>
         <Link href="/admin/marquee">Marquee</Link>
--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -4,15 +4,13 @@ export default function LoginPage() {
   return (
     <div className="flex min-h-[70vh] w-full items-center justify-center">
       <div className="w-full max-w-md ">
-        <div className="bg-neutral-900/90 py-3 rounded-lg">
-        <h1 className="text-center text-3xl font-bold text-white">
+        <h1 className="text-center text-3xl font-bold text-black dark:text-white">
           Admin Sign In
         </h1>
 
-        <p className="mt-2 text-center text-white/70">
+        <p className="mt-2 text-center text-black/70 dark:text-white/70">
           Sign in to access admin dashboard.
         </p>
-        </div>
         <AdminLoginForm />
       </div>
     </div>
