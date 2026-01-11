84ed73b48a8cf4b1f068ad9884219e761ac5e3ec
Vinayak <vinayaknautiyal38@gmail.com>
2026-01-11 13:05:34 +0530

    update

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-<div className="lg:pt-8 px-4">
+<div className="lg:pt-8 px-4 lg:px-28 ">
     <div className="flex flex-row justify-between items-center">
-      <div className="flex flex-row px-4 py-1 gap-3 bg-neutral-800/90 rounded-lg">
+      <div className="flex px-4 py-1 gap-3 bg-neutral-800/90 rounded-lg">
           <Link href="/admin">
-            Back to Admin
+            Admin Panel
           </Link>
       </div>
-      <div className="flex flex-col px-4 py-1 gap-1 bg-neutral-800/90 rounded-lg">
+      <div className="flex flex-col lg:flex-row lg:gap-3 px-4 py-1 gap-1 bg-neutral-800/90 rounded-lg">
         <Link href="/admin/products">Products</Link>
         <Link href="/admin/blogs">Blogs</Link>
         <Link href="/admin/marquee">Marquee</Link>
