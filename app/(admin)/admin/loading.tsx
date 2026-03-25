2cae4ab0826efbe34dd8a3710355a4d16a7f6e77
Vinayak <vinayaknautiyal38@gmail.com>
2026-03-25 21:36:29 +0530

    refactored loading animation

--- app/(admin)/admin/loading.tsx
+++ app/(admin)/admin/loading.tsx
@@ -2,8 +2,10 @@
 
 export default function LoadingPill() {
   return (
-    <div className="min-h-[70vh] flex items-center justify-center">
-      <div className="ribbon-loader" />
+    <div className="h-dvh w-full flex items-center justify-center">
+      <div className="-translate-y-10">
+        <div className="ribbon-loader" />
+      </div>
       <span className="sr-only">Loading</span>
     </div>
   );
--- app/(auth)/signup/loading.tsx
+++ app/(auth)/signup/loading.tsx
@@ -2,8 +2,10 @@
 
 export default function LoadingPill() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="ribbon-loader" />
+    <div className="h-dvh w-full flex items-center justify-center">
+      <div className="-translate-y-10">
+        <div className="ribbon-loader" />
+      </div>
       <span className="sr-only">Loading</span>
     </div>
   );
